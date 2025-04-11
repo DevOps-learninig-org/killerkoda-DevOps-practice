a0b0332802d8862b8db502252f0052f800a808ae
KoD <111854579+kodxxl@users.noreply.github.com>
2025-04-11 21:20:30 +0400

    Update foreground.sh

--- Linux/bash/scripts/foreground.sh
+++ Linux/bash/scripts/foreground.sh
@@ -1,3 +1,3 @@
-# If You want to read manual pages, run tmux, then press Ctrl+B then " (double quote)
-# Ctrl + B then Arrows, to navigate between panes (Ctrl+B Ctrl+DOWN/UP to resize)
-# Open manual in one pane and practice in other
+# If You want to read manual pages, run tmux, press Ctrl+B then " (double quote)
+# Ctrl + B then DOWN/UP, to navigate between panes (Ctrl+B Ctrl + DOWN/UP to resize)
+# Open manual in one pane and practice in other!
