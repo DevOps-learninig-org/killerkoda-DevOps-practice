4671f4149b24a4d04419e08549d95b836203cdc6
KoD <111854579+kodxxl@users.noreply.github.com>
2025-05-27 09:44:35 +0400

    Update foreground.sh

--- Linux/common/scripts/foreground.sh
+++ Linux/common/scripts/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tmux
-
 # You are running 'tmux' now, press Ctrl+b " (double quote) or % to split panes
 # Ctrl+b then DOWN/UP, to navigate between them (Ctrl+b Ctrl+DOWN/UP to resize)
 # Open manual in one pane and practice in other! (Ctrl+b x to close pane)
+
+tmux
