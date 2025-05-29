62b0939913085205143cf0e0a1e310cbfa2966e6
KoD <111854579+kodxxl@users.noreply.github.com>
2025-05-29 05:31:20 +0400

    Update foreground.sh

--- Linux/networking/scripts/foreground.sh
+++ Linux/networking/scripts/foreground.sh
@@ -1,1 +1,4 @@
-
+# If You want to read manual pages, run tmux, press Ctrl+b " (double quote) or %
+# Ctrl+b then DOWN/UP, to navigate between panes (Ctrl+b Ctrl+DOWN/UP to resize)
+# Ctrl-? for Shortcuts, or Ctrl-B :list-commands
+# Open manual in one pane and practice in other! (Ctrl+b x to close pane)
