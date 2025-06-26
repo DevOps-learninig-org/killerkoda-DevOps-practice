39f72ea2dac17c58002657556bd373a98b162cf2
Viacheslav Kod <xsrc@mail.ru>
2025-06-26 07:06:22 +0400

    Rename Linux subfolders

--- Linux/localhost/scripts/foreground.sh
+++
@@ -1,1 +0,0 @@
-tmux new\; split -v\; send 'man tmux' C-m\; select-pane -U\; send '# Use Ctrl+B ArrowUp - ArrowDown to choise tmux pane OR press Ctrl+B ? to help' C-m
--- Linux/services/scripts/background.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "alias }='touch IReadAllAndUndnderstood'" >> /etc/bash.bashrc
