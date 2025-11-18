c0518db0073b27b7353cc35c7084fe619c667d52
KoD <111854579+kodxxl@users.noreply.github.com>
2025-11-18 21:52:33 +0400

    Update foreground.sh

--- Linux/2-common/scripts/foreground.sh
+++ Linux/2-common/scripts/foreground.sh
@@ -2,4 +2,7 @@ tmux
 
 #You are running 'tmux' now, press Ctrl+b " (double quote) or % to split panes
 #Ctrl+b then DOWN/UP, to navigate between them (Ctrl+b Ctrl+DOWN/UP to resize)
+#Ctrl+b c - create window, Ctrl+b n OR p (next-prev window) Ctrl+b , - rename
 #Open manual in one pane and practice in other! (Ctrl+b x to close pane)
+#Ctrl+b d - detach into terminal, $ tmux ls; tmux attach 
+
